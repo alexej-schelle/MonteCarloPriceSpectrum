bb086e1bd873d9f1cb660049d5e43a64e8ca877b
alexej-schelle <94290038+alexej-schelle@users.noreply.github.com>
2022-08-07 10:37:00 +0200

    Add files via upload

--- monte_carlo_price_spectrum.py
+++ monte_carlo_price_spectrum.py
@@ -6,30 +6,31 @@
 #                                                                                                                                                                          #                 
 ############################################################################################################################################################################
 #                                                                                                                                                                          # 
-#                         This routine calculate a series of a most probable price spectrum in a game theory of purchase with possible prices defined by a certain price range [a, b]       #                                                                                                                                           #        																										 
+#                         This routine calculates a realisation of a Monte Carlo Price Spectrum in a game theory of purchase with possible prices defined by a certain     #
+#                         price range [a, b] assuming Poisson distributed events. In order to maximise your profit, choose a realisation with maximum price sum.           #                                                                                                                    #                                                                                                                                           #        																										 
 #                                                                                                                                                                          #             
-#  * :
-# 
-#   License Copyright:  Dr. A. Schelle, Bachschmidstr. 4, 87600 Kaufbeuren 
-#   License Type :      MIT license (2022)
-#   License Contact:    E-Mail : alexej.schelle@gmail.com
-# 
-#   ** : 
-#
-#   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files 
-#   (the "Software" bec_symmetry_breaking.py), to deal in the Software without restriction, including without limitation the rights to use, 
-#   copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
-#   furnished to do so, subject to the following conditions:
-# 
-#   The above copyright notice (*) and this permission notice (**) shall be included in all copies or substantial portions of the Software.
-# 
-#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-#   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
-#   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#                                                                                                                                                                                           
-#
-#                                                                                                                                                                                   
-################################################################################################################################################################################
+#  * :                                                                                                                                                                     #                 
+#                                                                                                                                                                          #                 
+#   License Copyright:  Dr. A. Schelle, Bachschmidstr. 4, 87600 Kaufbeuren                                                                                                 #
+#   License Type :      MIT license (2022)                                                                                                                                 #
+#   License Contact:    E-Mail : alexej.schelle@gmail.com                                                                                                                  #
+#                                                                                                                                                                          #             
+#   ** :                                                                                                                                                                   #             
+#                                                                                                                                                                          #                 
+#   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files                                       #
+#   (the "Software" bec_symmetry_breaking.py), to deal in the Software without restriction, including without limitation the rights to use,                                #
+#   copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is                                #
+#   furnished to do so, subject to the following conditions:                                                                                                               #
+#                                                                                                                                                                          #     
+#   The above copyright notice (*) and this permission notice (**) shall be included in all copies or substantial portions of the Software.                                #
+#                                                                                                                                                                          #                 
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,                    #
+#   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,          #
+#   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.             #
+#                                                                                                                                                                          #                 
+#                                                                                                                                                                          #     
+#                                                                                                                                                                          #             
+############################################################################################################################################################################
 
 import os
 import sys
@@ -46,92 +47,124 @@ sigma = 1.0 # Absolute price uncertainty
 N = 100 # Number of already registered reactions from trader (total number of trades)
 M = 50 # Number of proposals from buyer (positive feedback)
 
-price_start = 25.0*sigma # Start value of price sampling
 price_sample = 0.0*sigma # Variable price sampling
+price_old = 0.0*sigma # Dummy price variable
 
 price = [] # Collect possible prices
+price_final = [] # Collecto spectrum of optimal prices and start value of prices
+price_initial = [] # Collecto spectrum of optimal prices and start value of prices
 
 price_min = 25.0*sigma # Minimal price limit 
 price_max = 500.0*sigma # Maximal price limit
 
 L = 1000.0*sigma # Total price interval in units of sigma
-interaction_scale = 0.25*(price_max - price_min) # Correlation scale in units of LS (prefactor is defined by trade volume and number of trades)
+interaction_scale = 0.10*(price_max - price_min) # Correlation scale in units of LS (prefactor is defined by trade volume and number of trades)
 
 offset = 10.0 # Offset price value
-accuracy = 0.1 # Accuracy of the sample
+accuracy = 1.0 # Accuracy of the sample
 
 N_plus = 0 # Number of trades performed per realisation
 N_start = 100 # Number of trades performed initially by trader (set to initial value)
 
-number_of_iterations = 1000 # Count the number of iterations for the Monte-Carlo Algorithm
-number_of_trades = 5 # Number of trades you wish to perform
+number_of_iterations = 100 # Count the number of iterations for the Monte-Carlo Algorithm
+number_of_trades = 3 # Number of trades to be received from trader
+
+number_of_samples = 1000
 
 number_of_accepted_trades = []
 number_of_accepted_trades.append(0)
 
-for n in range(0, number_of_iterations):
+for k in range(0, number_of_samples):
 
-    N_plus = 0
-    print n
+    print 'Number of modelling steps for Monte Carlo Price Spectrum', k
 
-    for m in range(0, M) : # Average over number of trades
+    for n in range(0, number_of_iterations):
+
+        N_plus = 0
+        price_start = random.uniform(price_min,price_max)
+       
+        for m in range(0, M) : # Average over number of trades
     
-        for l in range(0, N) : # Average over number of offers
+            for l in range(0, N) : # Average over number of offers
 
-            price_sample = random.uniform(offset + 0.5*sigma, -offset + L - 0.5*sigma) # Generate random price sample in the interval [0,L]
+                price_sample = random.uniform(offset + 0.5*sigma, -offset + L - 0.5*sigma) # Generate random price sample in the interval [0,L]
+       
+                if (l == 0) : price_old = price_start
 
-            if (operator.gt(min(float(N_start)/float(M),1.00),random.uniform(0, 1.00)) and math.fabs(price_sample - random.uniform(price_min, price_max)) < accuracy) : # Condition for trade acceptance
+                if (operator.gt(min(float(l)/float(m+1),1.00),random.uniform(0, 1.00)) and math.fabs(price_sample - random.uniform(price_min, price_max)) < accuracy) : # Condition for trade acceptance
 
-                if (math.fabs(price_start - price_sample) < interaction_scale) : # Condition to choose prices proposed by the buyer
+                    if (math.fabs(price_old - price_sample) < interaction_scale) : # Condition to choose prices proposed by the buyer
 
-                    price.append(float(price_sample)) # Append prices to array
-                    price_start = price_sample # If price for trade is accepted, set start price to accepted trade price
+                        price.append(float(price_sample)) # Append prices to array
+                        price_old = price_sample # If price for trade is accepted, set start price to accepted trade price
 
-                    N_plus = N_plus + 1 # Count accepted trades
+                        N_plus = N_plus + 1 # Count accepted trades
 
-    number_of_accepted_trades.append(N_plus)
+        number_of_accepted_trades.append(N_plus)
 
-x = ['']*number_of_trades
-price_optimal = ['']*number_of_trades
-occurences = ['']*number_of_iterations
+    x = ['']*number_of_trades
+    price_optimal = ['']*number_of_trades
+    occurences = ['']*number_of_iterations
 
-for l in range(0, number_of_iterations):
+    occurences.append(0)
 
-    check_pass = 0
-    occurences[l] = 0
-
-    for k in range(0, number_of_trades):
-
-        x[k] = random.uniform(price_min, price_max)
-
-    for m in range(0, number_of_iterations):
+    for l in range(0, number_of_iterations):
 
         check_pass = 0
+        occurences[l] = 0
 
         for k in range(0, number_of_trades):
+
+            x[k] = random.uniform(price_min, price_max)
+
+        for m in range(0, number_of_iterations):
+
+            check_pass = 0
+
+            for k in range(0, number_of_trades):
         
-            if (float((math.fabs(x[k]) - float(price[number_of_accepted_trades[m] + k]))) < interaction_scale): check_pass = check_pass + 1    
+                if (float((math.fabs(x[k]) - float(price[number_of_accepted_trades[m] + k]))) < sigma): check_pass = check_pass + 1    
 
-        if (check_pass == number_of_trades):
+            if (check_pass == number_of_trades):
 
-            occurences[l] = occurences[l] + 1
+                occurences[l] = occurences[l] + 1
             
-result = np.argmax(occurences,axis=0)
+    result = np.argmax(occurences,axis=0)
+    max_occ = occurences[result]
 
-for k in range(0, number_of_trades):
+    price_mean = 0.0
+    price_old = 0.0
 
-    price_optimal[k] = price[number_of_accepted_trades[result] + k]    
+    for l in range(0, number_of_iterations):
 
-for l in range(number_of_trades) :
+        price_mean = 0.0
+
+        if (occurences[l] == max_occ):
+
+            for k in range(0, number_of_trades):
+
+                price_mean = price_mean + float(price[number_of_accepted_trades[l] + k])
+
+            price_mean = price_mean/float(number_of_trades)
+ 
+            if (price_mean >= price_old): 
+
+                for k in range(0, number_of_trades):
+
+                    price_optimal[k] = price[number_of_accepted_trades[l] + k]
+                    price_final.append(price_optimal[k])
+                    price_initial.append(price_start)
+            
+                price_old = price_mean
+
+
+    for l in range(number_of_trades) :
     
-    print 'TOP ' + str(l) + ' price value : ' + str(price_optimal[l])
+        print 'TOP ' + str(l) + ' price value : ' + str(price_optimal[l])
 
-print ' '
 
 plt.figure(1)
-plt.hist(price_optimal, bins = 250, normed = True)
+plt.hist2d(price_final, price_initial, bins = 250, normed = True)
 plt.tick_params(axis='both', which='major', labelsize = 16)
 plt.xlabel('price', fontsize = 18)
-plt.xlim([price_min, price_max])
-plt.ylim([0, 0.1])
 plt.savefig('/Users/dr.a.schelle/Desktop/Monte_Carlo_Price_Spectrum/fig_1.png')
